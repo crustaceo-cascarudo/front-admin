d2eddd558504881efd04f42be6c278b473106c09
Hugo <hugome2307@gmail.com>
2025-12-18 22:54:54 +0100

    feat(crustaceo-cascarudo/sprints#29): added creating ingredients

--- src/app/components/pages/p-ingredients/p-ingredients.ts
+++ src/app/components/pages/p-ingredients/p-ingredients.ts
@@ -1,8 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { Page } from '../../../models/page';
 import { HttpClientService } from '../../../services/http-client-service';
 import { Ingredient } from '../../../models/ingredient';
 import { CardIngredient } from "../../card-ingredient/card-ingredient";
+import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ProductInsert } from '../../../models/productInsert';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { CEditModal } from '../../ui/c-edit-modal/c-edit-modal';
+import { IngredientInsert } from '../../../models/ingredientInsert';
 
 @Component({
   selector: 'p-ingredients',
@@ -17,6 +23,11 @@ export class PIngredients {
   ingredients!: Ingredient[];
 
   http = inject(HttpClientService);
+
+  portal = new ComponentPortal(CEditModal);
+
+  private overlay = inject(Overlay);
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
     this.getData();
@@ -44,5 +55,31 @@ export class PIngredients {
       next: (datos) => this.ingredients = datos as unknown as Ingredient[],
       error: (error) => console.log('ERROR ' + error.status),
     })
+  }
+
+  openCreateModal() {
+    const config = new OverlayConfig({
+      positionStrategy: this.overlay.position().global().centerHorizontally().centerVertically(),
+      hasBackdrop: true,
+    });
+
+    const overlayRef = this.overlay.create(config);
+    const componentRef = overlayRef.attach(this.portal);
+    const newProduct: IngredientInsert = {
+      name: '',
+      price: 0,
+      image: '',
+    };
+
+    componentRef.instance.object = newProduct;
+    componentRef.instance.method = "POST";
+    componentRef.instance.apiurl = "/products";
+
+    componentRef.instance.saved.subscribe(() => {
+      this.getData();
+      overlayRef.detach();
+    });
+
+    overlayRef.backdropClick().pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => overlayRef.detach());
   }
 }
---
+++ src/app/models/ingredientInsert.ts
@@ -0,0 +1,5 @@
+export interface IngredientInsert {
+    name: string,
+    price: number,
+    image: string
+}
